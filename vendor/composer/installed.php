a627dba1e5fcc963538a475afec921e7ff294125
Ishii_Kaoru <shijingxun808@gmail.com>
2022-08-16 21:27:43 +0900

    heroku更新

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'feddd323fac370cd81732ca6c972398189fcbfba',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'feddd323fac370cd81732ca6c972398189fcbfba',
+            'dev_requirement' => false,
+        ),
+    ),
+);
